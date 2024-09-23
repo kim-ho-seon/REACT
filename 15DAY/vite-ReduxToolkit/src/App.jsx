d400f6e56c188ea93ada18423a4cbcaa43164668
kim-ho-seon <kmad910119@gmail.com>
2024-09-23 17:22:39 +0900

    DAY15 counter reduxtoolkit 실습완료

---
+++ 15DAY/vite-ReduxToolkit/src/App.jsx
@@ -0,0 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css'
+import { configureStore, createAsyncThunk, createSlice} from '@reduxjs/toolkit'
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState:0,
+  reducers: {
+    increment(state, action) {return state + 1},
+    decrement(state, action) {return state - 1}
+  }
+})
+
+const slowIncrementThunk = createAsyncThunk(
+  'counter/slowIncrement',
+  (value, {dispatch}) => {
+    setTimeout(() => {
+     dispatch( counterSlice.actions.increment())
+    }, 1000)
+  }
+)
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer
+  }
+}) 
+
+function App() {
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+  // console.log(counterSlice)
+  return (
+    <>
+      <div>Counter : {counter}</div>
+      <button onClick={() => {
+        dispatch( slowIncrementThunk() )
+      }}>+</button>
+      <button onClick={() => {
+        dispatch(counterSlice.actions.decrement())
+      }}>-</button>
+    </>
+  );
+}
+
+export default App
