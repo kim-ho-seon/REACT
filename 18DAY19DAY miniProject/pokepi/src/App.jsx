069f8377658177111c8d8f19bb5bd0dbddc0098d
kim-ho-seon <kmad910119@gmail.com>
2024-09-26 14:49:30 +0900

    DAY19 pokemon pageRouting

--- 18DAY19DAY miniProject/pokepi/src/App.jsx
+++ 18DAY19DAY miniProject/pokepi/src/App.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import './App.scss'
 import { useDispatch, useSelector } from 'react-redux'
 import { ffechMultiplePokemonByI } from './RTK/thunk'
+import { Link, Route, Routes } from 'react-router-dom'
+import Search from './pages/Search'
+import Detail from './pages/Detail'
+import Main from './pages/main'
+import Favorite from './pages/Favorit'
 
 function App() {
   const dispatch = useDispatch()
@@ -12,9 +17,23 @@ function App() {
   }, [])
 
   return(
-    <>
-
-    </>
+  <>
+    <h1 className='text-[40px] text-center'>포켓몬 도감</h1>
+    <nav className='flex gap-[10px] justify-center'>
+    <Link to={'/'}>메인</Link>
+    <Link to={'/detail/1'}>상세정보</Link>
+    <Link to={'/search'}>검색</Link>
+    <Link to={'/favorit'}>찜목록</Link>
+    </nav>
+    <main className='flex justify-center'>
+      <Routes>
+        <Route path={'/'} element={<Main />} />
+        <Route path={'/detail/:pokemonId'} element={<Detail />} />
+        <Route path={'/search'} element={<Search />} />
+        <Route path={'/favorit'} element={<Favorite />} />
+      </Routes>
+    </main>
+  </>
   )
 }
 
